dea2930dd127ae4167d8e943899ebd1c727badbf
Tom Dang <tomdangdev@gmail.com>
2026-02-16 13:28:10 +0700

    sup gang

---
+++ src/main/java/TomDang/example/roguePlugin/RoguePlugin.java
@@ -0,0 +1,17 @@
+package TomDang.example.roguePlugin;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class RoguePlugin extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
